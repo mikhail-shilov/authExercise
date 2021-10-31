ab1405c10ac756b6323c3ee169edc2729f316c80
Mikhail Shilov <mih.shilov@gmail.com>
2021-10-31 16:01:37 +0300

    Let's go...

--- client/components/dummy-view.jsx
+++ client/components/dummy-view.jsx
@@ -7,7 +7,7 @@ const Dummy = () => (
     <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
       <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
         <div className="mb-4">
-          <h1 className="text-grey-darkest">Todo List</h1>
+          <h1 className="text-grey-darkest">Not todo List</h1>
           <div className="flex mt-4">
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
--- client/components/head.jsx
+++ client/components/head.jsx
@@ -16,7 +16,7 @@ Head.propTypes = {
 }
 
 Head.defaultProps = {
-  title: 'skillcrucial.com'
+  title: 'authorize'
 }
 
 export default Head
